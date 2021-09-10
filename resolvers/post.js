5bd75df4a9bae56c62c3ebc3caa557492d0ef348
dembee111 <minion.ck@gmail.com>
2021-09-10 10:24:45 +0800

    mutation gql

--- resolvers/post.js
+++ resolvers/post.js
@@ -1,9 +1,28 @@
 const { gql } = require("apollo-server-express");
+const { posts } = require("../temp");
 
-const totalPosts = () => 42;
+// queries
+const totalPosts = () => posts.length;
+const allPosts = () => posts;
+
+// mutation
+const newPost = (parent, args) => {
+  // create a new post object
+  const post = {
+    id: posts.length + 1,
+    ...args.input,
+  };
+  // push new post object to posts array
+  posts.push(post);
+  return post;
+};
 
 module.exports = {
   Query: {
     totalPosts,
+    allPosts,
+  },
+  Mutation: {
+    newPost,
   },
 };
---
+++ temp.js
@@ -0,0 +1,16 @@
+const posts = [
+  {
+    id: 1,
+    title: "First post",
+    description: "First post description",
+  },
+  {
+    id: 2,
+    title: "Two post",
+    description: "two post description",
+  },
+];
+
+module.exports = {
+  posts,
+};
